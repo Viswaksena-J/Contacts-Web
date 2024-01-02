46bcf37ce3aa69c768df04453ea5cedf547d1679
Viswaksena Jayam <97895225+Viswaksena-J@users.noreply.github.com>
2024-01-03 01:47:56 +0530

    Thunder Client setup

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-console.log("Hi from Express")
+const express = require('express');
+const dotenv = require('dotenv').config()
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.get('/api/contacts',(req,res) => {
+    res.status(200).json({message:'Send all contacts'}) // send json
+})
+
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
+})
