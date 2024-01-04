168a20b2314a95085eb4f844aa6327b85e3cbc09
Viswaksena Jayam <97895225+Viswaksena-J@users.noreply.github.com>
2024-01-05 00:27:03 +0530

    Schema creation and CRUD expression to contacts

--- controller/contactController.js
+++ controller/contactController.js
@@ -1,11 +1,13 @@
 const asyncHandler = require('express-async-handler');
+const Contact = require('../models/contactModel');
 
 //@desc Get all contacts
 //@route GET /api/contacts
 //@ access public
 
 const getContacts = asyncHandler(async (req,res) => {
-    res.status(200).json({ message: `Get all contact` }); // use backticks for string interpolation
+    const contacts = await Contact.find()
+    res.status(200).json(contacts); // use backticks for string interpolation
 })
 
 //@desc Create New contacts
@@ -19,14 +21,26 @@ const createContact = asyncHandler(async (req,res) => {
         res.status(400);
         throw new Error("Please fill all the fields");
     }
+    const contact = await Contact.create({
+        name,
+        email,
+        phone
+    
+    })
+    res.status(201).json(contact);
 })
 
 //@desc getContact by id 
-//@route POST /api/contacts
+//@route GET /api/contacts/:id
 //@ access public
 
 const getContact = asyncHandler(async (req,res) => {
-    res.status(201).json({ message: `Create new contact` }); // use backticks for string interpolation
+    const contact = await Contact.findById(req.params.id)
+    if(!contact){
+        res.status(404);
+        throw new Error("Contact not found")
+    }
+    res.status(200).json(contact);  // use backticks for string interpolation
 })
 
 //@desc updateContact 
@@ -34,15 +48,36 @@ const getContact = asyncHandler(async (req,res) => {
 //@ access public
 
 const updateContact = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: `Update contact for ${req.params.id}` }); // use backticks for string interpolation
+    const contact = await Contact.findById(req.params.id)
+    if(!contact){
+        res.status(404);
+        throw new Error("Contact not found")
+    }
+    const updatedContact = await Contact.findByIdAndUpdate(
+        req.params.id,
+        req.body,{
+            new:true,
+        }
+    )
+    res.status(200).json(updatedContact); // use backticks for string interpolation
 })
 
 //@desc deleteContact 
 //@route DELETE /api/contacts/:id
 //@ access public
 
-const deleteContact = asyncHandler(async  (req, res) => {
-    res.status(200).json({ message: `Delete contact for ${req.params.id}` }); // use backticks for string interpolation
-})
+const deleteContact = asyncHandler(async (req, res) => {
+    const contact = await Contact.findById(req.params.id);
+    if (!contact) {
+      res.status(404);
+      throw new Error("Contact not found");
+    }
+    if (contact.user_id.toString() !== req.user.id) {
+      res.status(403);
+      throw new Error("User don't have permission to update other user contacts");
+    }
+    await Contact.deleteOne({ _id: req.params.id });
+    res.status(200).json(contact);
+});
 
 module.exports = {getContacts,getContact,createContact,updateContact,deleteContact};
